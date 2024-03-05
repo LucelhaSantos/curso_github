cd8a3a5f2e893ffee09ba3de08e22f2d2aeefe06
Lucelha Santos <lucelha_santos@msn.com>
2024-03-05 17:46:31 -0300

    Adicionando sistema de mensagens

--- cadastro.js
+++ cadastro.js
@@ -6,3 +6,11 @@ return email
 }
 
 //pegando um e-mail e cadastrando na plataforma
+
+function mostraMensagem() {
+    return Bem Vindo ao Sistema
+    
+    
+    }
+    
+    //adicionando função de mensagem
