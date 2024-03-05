25c283a5aa220c98e3ab590aa6251f04e4923d36
Lucelha Santos <lucelha_santos@msn.com>
2024-03-05 16:30:24 -0300

    Craindo a sergunda versão do sistema

--- home.js
+++ home.js
@@ -2,3 +2,9 @@ function mostraNome (nome) {
   return nome
 
 }
+function mostraSobrenome (sobreNome){
+  return sobreNome
+
+}
+
+//finalizando sistema da home
--- login.js
+++ login.js
@@ -2,3 +2,9 @@
 //Trabalhando no Login
 
 // Finalizando sistema de Login
+
+function Login(nome, email){
+    return "Bem Vindo";
+}
+
+// Seja Bem Vindo
